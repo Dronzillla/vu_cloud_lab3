f0d632a2df8f1a4d02a44933748361c691184404
Dominykas <110461307+Dronzillla@users.noreply.github.com>
2025-09-26 15:01:30 +0300

    Change database to postgre

--- config/config.py
+++ config/config.py
@@ -1,19 +1,31 @@
+import os
+
+
 class Config:
-    SECRET_KEY = "VerySecretKey"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///./app.db"
+    SECRET_KEY = os.getenv("SECRET_KEY", "VerySecretKey")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # disable warning globally
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    # Development DB from env variable
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", "postgresql://postgres:example@localhost:5432/todos_db"
+    )
 
 
 class ProductionConfig(Config):
     DEBUG = False
+    # Production DB will be provided via DATABASE_URL env variable
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///./test.db"
+    # Separate database for testing
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "TEST_DATABASE_URL",
+        "postgresql://postgres:example@localhost:5432/todos_test_db",
+    )
     WTF_CSRF_ENABLED = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SERVER_NAME = "127.0.0.1:5000"
