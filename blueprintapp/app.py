e67897029d5a7e1723218f18b20eae14ad0a758d
Dominykas <110461307+Dronzillla@users.noreply.github.com>
2025-09-27 13:36:35 +0300

    Create tables without migrations

--- blueprintapp/app.py
+++ blueprintapp/app.py
@@ -24,10 +24,13 @@ def create_app(config_class="config.config.DevelopmentConfig"):
     app.register_blueprint(todos, url_prefix="/todos")
     app.register_blueprint(api, url_prefix="/api")
 
-    migrate = Migrate(app, db)
+    # migrate = Migrate(app, db)
     # To create db go to blueprintapp folder where app.py is
     # flask db init
     # flask db migrate
     # flask db upgrade
+
+    with app.app_context():
+        db.create_all()
 
     return app
